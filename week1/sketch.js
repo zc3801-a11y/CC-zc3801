e3653a584a11f66dcc01b91dd80473ade052271e
zc3801-a11y <zc3801@nyu.edu>
2026-09-09 11:57:25 -0400

    Update sketch.js

--- week1/sketch.js
+++ week1/sketch.js
@@ -22,7 +22,7 @@ function setup() {
   // Seed with millis() so each page load gives a different quote.
   randomSeed(millis());
   textSize(32);
-  fill(240, 255, 10);
+  fill(10, 255, 10);
   // calls the function to pick a quote
   pickQuote();
 }
@@ -33,7 +33,7 @@ function pickQuote() {
 }
 
 function draw() {
-  background(255, 0, 125); // set the background color
+  background(255, 0, 250); // set the background color
   drawQuote();  // draw the quote on screen
 }
 
