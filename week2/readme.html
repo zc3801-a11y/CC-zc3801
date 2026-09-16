ab40abae5fb2ad30c63500a466b20f1ab9cf9439
zc3801-a11y <zc3801@nyu.edu>
2026-09-16 12:25:55 -0400

    week2 process drawing
    Inspired by the Bauhaus style, using simple geometric shapes and a red, yellow, blue, and black color palette. Each mouse click randomly changes the shapes’ colors, positions, sizes, rotations, and background, creating a new composition.

---
+++ week2/readme.html
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <script src="sketch.js"></script>
+  <title>Sketch Name</title> <!--Update your title here-->
+  <!--Add style to your code within the style tag <style></style>
+  Read the W3Schools CSS Intro for more: https://www.w3schools.com/css/css_intro.asp 
+  -->
+  <style>
+    body {
+      font-family: sans-serif;
+      margin: 2em;
+      /*padding: 1em;*/
+      color: #000000;
+      background-color: #f0f0f0;
+    }
+
+    a {
+      color: rgb(0,0,255);
+    }
+
+    a:hover {
+      text-decoration: dashed;
+    }
+
+    h1 {
+      /* Insert your own style here */
+    }
+
+    h2 {
+      /* Insert your own style here */
+    }
+  </style>
+</head>
+
+<body>
+    <h1>Sketch Name</h1>
+    <p>Add your description here.</p>
+    <p><a href="index.html" target="_blank">Link to sketch</a></p>
+
+   <h2>Resources</h2>
+   <ul>
+    <li><a href="http://example.com" target="_blank">One reference</a></li>
+    <li><a href="http://example.com" target="_blank">Another reference</a></li>
+    <li><a href="http://example.com" target="_blank">And another reference</a></li>
+   </ul>
+
+    <!--Replace with your own image-->
+    <!--You can limit the size of your displayed image using "height" or "width" -->
+    <img src="media/image.jpg" height="400px" alt="This the alt description of this image. This is a picture of a cat."></img>
+</body>
+</html>
---
+++ week2/sketch.js
@@ -0,0 +1,78 @@
+function setup() {
+  createCanvas(510, 650);
+  angleMode(DEGREES);
+
+  generateDrawing();
+}
+
+function draw() {
+  background();
+}
+
+function mousePressed() {
+  generateDrawing();
+}
+//background colors 
+function generateDrawing() {
+  let backgroundColors = [[245, 230, 184], [238, 223, 166], [247, 237, 204], [232, 213, 154],[255, 243, 196]];
+  let randomBackgroundColors = shuffle(backgroundColors);
+
+//shapes colors
+let shapeColors = [[189, 13, 0], [255, 205, 41], [30, 197, 252], [0, 0, 0]];
+let randomShapeColors = shuffle(shapeColors);
+
+//positions
+let positions = [[width * 0.25, height * 0.25], [width * 0.75, height * 0.25], [width * 0.25, height * 0.75], [width * 0.75, height * 0.25], [width * 0.7, height * 0.7]];
+let randomPositions = shuffle(positions);
+
+//sizes
+let sizes = [0.5, 0.7, 0.85, 1];
+let randomSizes = shuffle(sizes);
+
+//draw background
+  background(randomBackgrounds[0]);
+  noStroke();
+
+//rectangles
+push ();
+rectMode(CENTER);
+translate(randomPositions[0][0], randomPositions[0][1]);
+rotate (random(-45, 45));
+scale(randomSizes[0]);
+fill(randomShapeColors[0]);
+rect(0, 0, maxSize, maxSize * 0.5);
+pop();
+
+//circles
+push ();
+translate(randomPositions[1][0], randomPositions[1][1]);
+scale(randomSizes[1]);
+fill(randomShapeColors[1]);
+ellipse(0, 0, maxSize);
+pop();
+
+//triangles
+push ();
+translate(randomPositions[2][0], randomPositions[2][1]);
+rotate (random(-45, 45));
+scale(randomSizes[2]);
+fill(randomShapeColors[2]);
+triangle(0, -maxSize / 2, -maxSize / 2, maxSize / 2,maxSize / 2, maxSize / 2);
+pop();
+
+//black bar
+push ();
+rectMode(CENTER);
+translate(randomPositions[3][0], randomPositions[3][1]);
+rotate (random(-70, 70));
+scale(randomSizes[3]);
+fill(randomShapeColors[3]);
+rect(0, 0, maxSize, maxSize * 0.18);
+pop();
+
+ 
+
+
+
+
+}
